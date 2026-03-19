54f16da7c2730f95a317ea600c33a317f1c2a2fd
muhammedshaheervp36-ui <muhammedshaheervp36@gmail.com>
2026-03-19 16:33:56 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const dom = new JSDOM(<!DOCTYPE html><div id='app'></div>);
+const document = dom.window.document;
+const app = document.getElementById('app');
+try {
+  app.innerHTML = \${(Get-Content "d:\HIYARA WEB\index.html" -Raw) -match '(?s)app.innerHTML = (.*?);' | Out-Null; [1] -replace '', '\'}\;
+  console.log(document.getElementById('cartItemsContainer'));
+} catch(e) { console.error('Error:', e.message); }
